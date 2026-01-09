e06487b5c8bdae5d9562e42cc0654e941218f0ec
muralisite <nadipenamurali13@gmail.com>
2026-01-09 21:57:53 +0530

    fix errors

--- checkout-page/src/App.jsx
+++ checkout-page/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Checkout from './pages/Checkout';
 
+
 function App() {
   return (
     <Router>
